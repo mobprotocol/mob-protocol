5dc8cd0517d6e503a76f101dd12fe68b61366f64
Sean Pollock <seanpollock3344@gmail.com>
2017-07-15 16:00:15 -0400

    message hash test

--- test/signature.js
+++ test/signature.js
@@ -1,5 +1,23 @@
 const eth = require('ethereumjs-util')
 
-const { ecsign } = eth
+const { ecsign, hashPersonalMessage } = eth
 
 console.log('ecsign', ecsign)
+
+console.log('hashPersonaleMessage', hashPersonalMessage)
+
+
+function signTest(obj) {
+  return new Promise((resolve, reject) => {
+    return hashMessage(JSON.stringify(obj))
+    .then((hash) => {
+      console.log('hash', hash)
+    })
+  })
+}
+
+function hashMessage(msg) {
+  return new Promise((resolve, reject) => {
+    resolve(hashPersonalMessage(msg))
+  })
+}
