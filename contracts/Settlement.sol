03c1ad5735ec77416a031d575a22a018eb912c58
Sean Pollock <seanpollock3344@gmail.com>
2017-07-14 22:50:23 -0400

    fixing naming conflict

--- contracts/Settlement.sol
+++ contracts/Settlement.sol
@@ -6,13 +6,17 @@ contract Settlement {
   address public tokenB;
   mapping(address => mapping(bytes32 => uint)) public orders;
 
-  function Settlement(bytes32 permutationID, address TokenA, address TokenB) {
-    permutationID = permutationID;
-    tokenA = tokenA;
-    tokenB = tokenB;
+  function Settlement(bytes32 _permutationID, address _tokenA, address _tokenB) {
+    permutationID = _permutationID;
+    tokenA = _tokenA;
+    tokenB = _tokenB;
   }
 
   function broadcast(bytes32 order, uint sellAmount) {
     orders[msg.sender][order] = sellAmount;
   }
 }
+/**
+Testing Inputs:
+
+"0x8550e43dd8cc744ac8ccd7b7d160484b9a338c0f", "0x8550e43dd8cc744ac8ccd7b7d160484b9a338c0f", "0x8550e43dd8cc744ac8ccd7b7d160484b9a338c0f"
