6e6d4088b6446ef73f0179bcbb9a7d28ec17f4e2
Sean Pollock <seanpollock3344@gmail.com>
2017-07-14 22:11:32 -0400

    defining globals

---
+++ contracts/Settlement.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.11;
+
+contract Settlement {
+  bytes32 public permutationID;
+  address public tokenA;
+  address public tokenB;
+
+  function Settlement(bytes32 permutationID, address TokenA, addressTokenB) {
+
+  }
+}
