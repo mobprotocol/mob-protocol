c1df586fc44ab7debf3ddded453ffa92f1952b5c
Sean Pollock <seanpollock3344@gmail.com>
2017-07-14 22:25:54 -0400

    Adding broadcast function

--- contracts/Settlement.sol
+++ contracts/Settlement.sol
@@ -4,11 +4,15 @@ contract Settlement {
   bytes32 public permutationID;
   address public tokenA;
   address public tokenB;
-  mapping(address => mapping(bytes32 => uint));
+  mapping(address => mapping(bytes32 => uint)) public orders;
 
   function Settlement(bytes32 permutationID, address TokenA, address TokenB) {
     permutationID = permutationID;
     tokenA = tokenA;
     tokenB = tokenB;
+  }
+
+  function broadcast(bytes32 order, uint sellAmount) {
+    orders[msg.sender][order] = sellAmount;
   }
 }
