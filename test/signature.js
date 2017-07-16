39ccb227d3104093a3fbba8abc2c07e0e09427c3
Sean Pollock <seanpollock3344@gmail.com>
2017-07-16 11:04:59 -0400

    Switching to hex encoding

--- test/signature.js
+++ test/signature.js
@@ -6,14 +6,24 @@ function signTest(obj) {
   return new Promise((resolve, reject) => {
     return hashMessage(JSON.stringify(obj))
     .then((hash) => {
-      console.log('hash', hash)
+      return signOrder(hash.toString('hex'), '65de830423de93cba6f05c4e819f757434d8c6228b4498f5c34cafac6bcd0c3d')
+    }).then((res) => {
+      console.log('res', res)
+    }).catch((err) => {
+      reject(err)
     })
   })
 }
 
 function hashMessage(msg) {
   return new Promise((resolve, reject) => {
-    resolve(hashPersonalMessage(Buffer.from(msg, 'utf8')))
+    resolve(hashPersonalMessage(Buffer.from(msg, 'hex')))
+  })
+}
+
+function signOrder(hash, pk) {
+  return new Promise((resolve, reject) => {
+    resolve(ecsign(Buffer.from(hash, 'hex'), Buffer.from(pk, 'hex')))
   })
 }
 
