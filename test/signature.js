38e2c287a79d0fce0e7823e7e9283c7cdbaa734b
Sean Pollock <seanpollock3344@gmail.com>
2017-07-17 13:53:31 -0400

    testing ecrecover in js

--- test/signature.js
+++ test/signature.js
@@ -1,14 +1,23 @@
 const eth = require('ethereumjs-util')
 
-const { ecsign, hashPersonalMessage } = eth
+const { ecsign, hashPersonalMessage, ecrecover } = eth
 
 function signTest(obj) {
   return new Promise((resolve, reject) => {
+    let hashedMsg
     return hashMessage(JSON.stringify(obj))
     .then((hash) => {
+      console.log('hash', hash.toString('hex'))
+      hashedMsg = hash
       return signOrder(hash.toString('hex'), '65de830423de93cba6f05c4e819f757434d8c6228b4498f5c34cafac6bcd0c3d')
     }).then((res) => {
-      console.log('res', res)
+      console.log('v', res.v)
+      console.log('r', res.r.toString('hex'))
+      console.log('s', res.s.toString('hex'))
+      return recover(hashedMsg, res.v, res.r, res.s)
+    }).then((publicKey) => {
+      console.log('publicKey', publicKey.toString('hex'))
+      const 
     }).catch((err) => {
       reject(err)
     })
@@ -24,6 +33,12 @@ function hashMessage(msg) {
 function signOrder(hash, pk) {
   return new Promise((resolve, reject) => {
     resolve(ecsign(Buffer.from(hash, 'hex'), Buffer.from(pk, 'hex')))
+  })
+}
+
+function recover(hash, v, r, s) {
+  return new Promise((resolve, reject) => {
+    resolve(ecrecover(hash, v, r, s))
   })
 }
 
