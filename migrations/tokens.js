149a6e6637a1c396c35dcf3198704b7c184e4922
Sean Pollock <seanpollock3344@gmail.com>
2017-07-18 15:29:51 -0400

    adding token params

---
+++ migrations/tokens.js
@@ -0,0 +1,12 @@
+export const tokens = [
+  {
+    name: 'Mob',
+    ticker: 'MOB',
+    supply: 1000000000
+  },
+  {
+    name: 'Basic Attention Token',
+    ticker: 'BAT',
+    supply: 1000000000
+  }
+]
