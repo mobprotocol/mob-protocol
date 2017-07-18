68263cf62d5cd6f5a812b548cf31711f8d2c6eef
Sean Pollock <seanpollock3344@gmail.com>
2017-07-18 09:58:47 -0400

    getting msg hash

--- test/web3sign.js
+++ test/web3sign.js
@@ -1,18 +1,19 @@
 var Web3 = require('web3')
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+const sha3 = require('solidity-sha3').default
 
 function getAddress() {
   return new Promise((resolve, reject) => {
     web3.eth.getAccounts((err, res) => {
       if (err) { reject (err)}
-      resolve(res)
+      resolve(res[0])
     })
   })
 }
 
 function hashData() {
   return new Promise((resolve, reject) => {
-
+    resolve(sha3("hello world"))
   })
 }
 
@@ -29,7 +30,10 @@ function test() {
     return getAddress()
     .then((res) => {
       address = res
+      return hashData()
+    }).then((res) => {
       console.log('res', res)
+      hashedData = res
     })
   })
 }
