343e2221eea15cf8857f8561c211c5ab5bacc2d8
Sean Pollock <seanpollock3344@gmail.com>
2017-07-18 09:45:11 -0400

    scaffolding web3 sign test

---
+++ test/web3sign.js
@@ -0,0 +1,31 @@
+var Web3 = require('web3')
+var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+
+function getAddress() {
+  return new Promise((resolve, reject) => {
+
+  })
+}
+
+function hashData() {
+  return new Promise((resolve, reject) => {
+
+  })
+}
+
+function signature() {
+  return new Promise((resolve, reject) => {
+
+  })
+}
+
+function test() {
+  return new Promise((resolve, reject) => {
+    let  addresss
+    let hashedData
+    return getAddress()
+    .then(() => {
+      
+    })
+  })
+}
