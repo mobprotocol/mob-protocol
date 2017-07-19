066018576c4c6d40bf58e1d7b060c79fc08d167c
Sean Pollock <seanpollock3344@gmail.com>
2017-07-18 22:34:52 -0400

    latest migrations

---
+++ migrations/2_deploy_token_contracts.js
@@ -0,0 +1,29 @@
+// module.exports = function(deployer) {
+//   deployer.deploy(ConvertLib);
+//   deployer.autolink();
+//   deployer.deploy(MetaCoin);
+// };
+const SafeMath = artifacts.require("SafeMath")
+const Token = artifacts.require("Token")
+
+module.exports = function(deployer) {
+  const tokens = [
+    {
+      name: 'Mob',
+      ticker: 'MOB',
+      supply: 1000000000
+    },
+    {
+      name: 'Basic Attention Token',
+      ticker: 'BAT',
+      supply: 1000000000
+    }
+  ]
+
+  deployer.deploy(SafeMath)
+  deployer.deploy(Token)
+  tokens.forEach((token) => {
+    console.log('token', token)
+    Token.new(token.name, token.ticker, token.supply)
+  })
+}
