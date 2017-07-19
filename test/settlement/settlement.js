847979f294dcfb902235ae1a97cc76b0307a3e89
Sean Pollock <seanpollock3344@gmail.com>
2017-07-19 14:21:05 -0400

    permutationID passing

--- test/settlement/settlement.js
+++ test/settlement/settlement.js
@@ -21,8 +21,11 @@ contract('Settlement' , (accounts) => {
     const addressA = '0x2da664251cdff1ef96471d5570d6b7d3687b4516'
     const addressB = '0x6846e948d8b1ec25bb99dedf821b0d658e226595'
     const permutationID = calculatePermutationID(addressA, addressB)
-    const instance = Settlement.deployed(permutationID, addressA, addressB)
-    const contractPermID = instance.permutationID.call(accounts[0])
-    assert.equal(contractPermID, permutationID, "permutation id is wrong")
+    Settlement.new(permutationID, addressA, addressB)
+    .then((instance) => {
+      return instance.permutationID.call()
+    }).then((permutation) =>{
+      assert.equal(permutation, permutationID, "permutation id is wrong")
+    })
   })
 })
