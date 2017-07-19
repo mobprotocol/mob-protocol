692eca13b828531648891fc3a1043ad5927bdc43
Sean Pollock <seanpollock3344@gmail.com>
2017-07-19 10:50:42 -0400

    starting settement tests

---
+++ test/settlement.js
@@ -0,0 +1,14 @@
+/**
+  unit tess for Settlement.sol
+*/
+
+const Settlement = artifacts.require("Settlement")
+
+contract('Settlement' , (accounts) => {
+  // it("Should return false given a public address and the wrong vrs signature", () => {
+  //   return Settlement.deployed().then((instance) => {
+  //     return instance.verifySignature()
+  //   })
+  // })
+  console.log('accounts', accounts)
+})
