deed7ec08a15265b6742bca503b6f8e832722d85
Sean Pollock <seanpollock3344@gmail.com>
2017-07-19 14:05:23 -0400

    assertion for perm id

--- test/settlement/settlement.js
+++ test/settlement/settlement.js
@@ -17,10 +17,12 @@ contract('Settlement' , (accounts) => {
   //     console.log('signature')
   //   })
   // })
-  it("Should deploy contract and define globals permutation id and token addresses", () => {
+  it("Should deploy contract and define global permutation id", () => {
     const addressA = '0x2da664251cdff1ef96471d5570d6b7d3687b4516'
     const addressB = '0x6846e948d8b1ec25bb99dedf821b0d658e226595'
     const permutationID = calculatePermutationID(addressA, addressB)
     const instance = Settlement.deployed(permutationID, addressA, addressB)
+    const contractPermID = instance.permutationID.call(accounts[0])
+    assert.equal(contractPermID, permutationID, "permutation id is wrong")
   })
 })
