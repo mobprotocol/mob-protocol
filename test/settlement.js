ce8acf4bd78d048c68403d6b4179293e404d3463
Sean Pollock <seanpollock3344@gmail.com>
2017-07-19 10:56:48 -0400

    creating contract instance

--- test/settlement.js
+++ test/settlement.js
@@ -5,10 +5,14 @@
 const Settlement = artifacts.require("Settlement")
 
 contract('Settlement' , (accounts) => {
-  // it("Should return false given a public address and the wrong vrs signature", () => {
-  //   return Settlement.deployed().then((instance) => {
-  //     return instance.verifySignature()
-  //   })
-  // })
+  it("Should return false given a public address and the wrong vrs signature", () => {
+    let instance
+    return Settlement.deployed()
+    .then((inst) => {
+      instance = inst
+      console.log('instance', instance)
+      // return generateSignature(accounts[0], "hello world")
+    })
+  })
   console.log('accounts', accounts)
 })
