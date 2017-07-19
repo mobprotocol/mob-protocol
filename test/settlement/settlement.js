f8a4771dde6f601c484812e2d17ea9566f62dcd8
Sean Pollock <seanpollock3344@gmail.com>
2017-07-19 13:45:52 -0400

    deploying contract with params

--- test/settlement/settlement.js
+++ test/settlement/settlement.js
@@ -4,15 +4,23 @@
 
 const Settlement = artifacts.require("Settlement")
 
+const { generateSignature, claculatePermutationID } = require('./utils.js')
+
 contract('Settlement' , (accounts) => {
-  it("Should return false given a public address and the wrong vrs signature", () => {
-    let instance
-    return Settlement.deployed()
-    .then((inst) => {
-      instance = inst
-      console.log('instance', instance)
-      // return generateSignature(accounts[0], "hello world")
-    })
+  // it("Should return false given a public address and the wrong vrs signature", () => {
+  //   let instance
+  //   return Settlement.deployed()
+  //   .then((inst) => {
+  //     instance = inst
+  //     return generateSignature(accounts[0], "hello world")
+  //   }).then((signature) => {
+  //     console.log('signature')
+  //   })
+  // })
+  it("Should deploy contract and define globals permutation id and token addresses", () => {
+    const addressA = '0x2da664251cdff1ef96471d5570d6b7d3687b4516'
+    const addressB = '0x6846e948d8b1ec25bb99dedf821b0d658e226595'
+    const permutationID = calculatePermutationID(addressA, addressB)
+    const instance = Settlement.deployed(permutationID, addressA, addressB)
   })
-  console.log('accounts', accounts)
 })
