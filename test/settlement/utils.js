d55c235c025a2f5f2ce6bd736c0ebfdbb0c152d4
Sean Pollock <seanpollock3344@gmail.com>
2017-07-19 13:42:53 -0400

    adding permutation id method to utils

--- test/settlement/utils.js
+++ test/settlement/utils.js
@@ -4,16 +4,37 @@ const { sha3 } = ethUtil
 const Web3 = require('web3')
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
 
-export function generateSignature(publicAddress, msg) {
-  return new Promise((resolve, reject) => {
+function generateSignature(publicAddress, msg) {
+  return new Promise(async (resolve, reject) => {
     const hashedMsg = sha3(msg)
-    return signOrder(publicAddress, hashedMsg)
+    console.log('hashedMsg', hashedMsg)
+    // const signature = await signOrder(address, hash)
 
+    resolve(hashedMsg)
   })
 }
 
-function signOrder(address, hash) {
+async function signOrder(address, hash) {
   return new Promise((resolve, reject) => {
-
+    web3.eth.sign(address, hash, (err, res) => {
+      if (err) { reject(err) }
+      resolve(res)
+    })
   })
+}
+
+function calculatePermutationID(addressA, addressB) {
+  const ordering = addressA.localeCompare(addressB)
+  let concated
+  if(ordering == 0) {
+    concated = `${addressA}${addressB}`
+  } else {
+    concated = `${addressB}${addressA}`
+  }
+  return '0x' + sha3(concated).toString('hex')
+}
+
+module.exports = {
+  generateSignature,
+
 }
