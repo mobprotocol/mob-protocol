bd8e7ae8453f31f35746c3ad352acf3e0fdf59a2
Sean Pollock <seanpollock3344@gmail.com>
2017-07-18 22:00:26 -0400

    updating network config to truffle 3.0 standards

---
+++ truffle.js
@@ -0,0 +1,29 @@
+module.exports = {
+  build: {
+    "index.html": "index.html",
+    "app.js": [
+      "javascripts/app.js"
+    ],
+    "app.css": [
+      "stylesheets/app.css"
+    ],
+    "images/": "images/"
+  },
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*"
+    },
+    staging: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*"
+    },
+    ropsten: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*"
+    }
+  }
+};
