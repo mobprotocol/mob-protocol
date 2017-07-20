25a4cf8664d97c62cf021ddca29fa75b11403448
Sean Pollock <seanpollock3344@gmail.com>
2017-07-20 13:12:32 -0400

    changing return type to address for testing

--- contracts/Settlement.sol
+++ contracts/Settlement.sol
@@ -21,14 +21,10 @@ contract Settlement {
     return true;
   }
 
-  function verifySignature(bytes32 msg, uint8 v, bytes32 r, bytes32 s, address seller) returns (bool) {
+  function verifySignature(bytes32 msg, uint8 v, bytes32 r, bytes32 s, address seller) returns (address) {
     bytes memory prefix = "\x19Ethereum Signed Message:\n32";
     bytes32 msgHash = sha3(prefix, msg);
-    address signer = ecrecover(msgHash, v, signature[0], signature[1]);
-    if (signer == seller) {
-      return true;
-    } else {
-      return false;
-    }
+    address signer = ecrecover(msgHash, v, r, s);
+    return signer;
   }
 }
