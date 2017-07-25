d25fb7fece2e59e8b6fdb024c118699dea5e9e8b
Sean Pollock <seanpollock3344@gmail.com>
2017-07-25 16:10:39 -0400

    adding first test for match transfers

--- test/settlement/settlement.js
+++ test/settlement/settlement.js
@@ -102,7 +102,7 @@ contract('Settlement' , (accounts) => {
 
   it('Should verify that approval balance is greater than order amount', () => {
     let settlementContract
-    let tokenContract
+    let tokenContractle
     const addressA = '0x2da664251cdff1ef96471d5570d6b7d3687b4516'
     const addressB = '0x6846e948d8b1ec25bb99dedf821b0d658e226595'
     const permutationID = calculatePermutationID(addressA, addressB)
@@ -118,6 +118,49 @@ contract('Settlement' , (accounts) => {
       return settlementContract.verifyAllowance.call(tokenContract.address, accounts[0], 500)
     }).then((res) => {
       assert.equal(res, true)
+    })
+  })
+
+  it('Should transfer tokens given corrent order and signature', () => {
+    let settlementContract
+    let tokenContract
+    let signature
+    const addressA = '0x2da664251cdff1ef96471d5570d6b7d3687b4516'
+    const addressB = '0x6846e948d8b1ec25bb99dedf821b0d658e226595'
+    const permutationID = calculatePermutationID(addressA, addressB)
+    const order = {
+      seller: accounts[0],
+      token: addressA,
+      quantity: 10,
+      price: 10,
+      permutationID: permutationID,
+    }
+    const orderHash = hashOrder(order)
+    Settlement.new(permutationID, addressA, addressB)
+    .then((inst) => {
+      settlementContract = inst
+      return Token.new('Mob', 'MOB', 1000)
+    }).then((inst) => {
+      tokenContract = inst
+      return inst.approve(settlementContract.address, 500)
+    }).then((res) => {
+      return generateSignature(accounts[0], orderHash)
+    }).then((sig) => {
+      signature = sig
+      return settlementContract.atomicMatch(
+        orderHash,
+        order.seller,
+        order.token,
+        order.quantity,
+        order.price,
+        sig[0],
+        sig[1],
+        sig[2],
+        accounts[2],
+        order.quantity
+      )
+    }).then((res) => {
+      console.log('res', res)
     })
   })
 
