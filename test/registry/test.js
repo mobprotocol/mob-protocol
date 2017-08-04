04b8a9a4d9d30a20451c707f23189ded9e1bd7df
Sean Pollock <seanpollock3344@gmail.com>
2017-08-04 13:38:20 -0400

    scaffolding out registry tests

---
+++ test/registry/test.js
@@ -0,0 +1,14 @@
+const Registry = artifiacts.require("Registry")
+const Token = artifacts.require("Token")
+const Settlement = artifacts.require("Settlement")
+
+const { calculatePermutationID } = require('../settlement/utils.js')
+
+const addressA = '0x2da664251cdff1ef96471d5570d6b7d3687b4516'
+const addressB = '0x6846e948d8b1ec25bb99dedf821b0d658e226595'
+const permutationID = calculatePermutationID(addressA, addressB)
+
+contract('Registry', (accounts) => {
+  it('Should deploy and add settlement contract to registry', () => {
+  })
+})
