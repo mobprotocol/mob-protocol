e5f6464e756cf8dd0afd12d3fa179c1256a138b7
Sean Pollock <seanpollock3344@gmail.com>
2017-08-04 13:32:16 -0400

    adding assembly to check if contract exists

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -3,15 +3,29 @@ pragma solidity ^0.4.11;
 contract Registry {
   mapping(bytes32 => address) public registry;
 
-  function registerPermutation(bytes32 permutationID, address settlementContract) returns (bool) {
-    registry[permutationID] = settlementContract;
-  }
-
   function permutationExists(bytes32 permutationID) returns (bool) {
     if(registry[permutationID] == 0) {
       return true;
     } else {
       return false;
     }
+  }
+
+  function contractExists(address settlementContract) returns (bool) {
+    uint length;
+    assembly {
+      length := extcodesize(settlementContract)
+    }
+    if (length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function registerPermutation(bytes32 permutationID, address settlementContract) returns (bool) {
+    require(permutationExists(permutationID));
+    require(contractExists(settlementContract));
+    registry[permutationID] = settlementContract;
   }
 }
